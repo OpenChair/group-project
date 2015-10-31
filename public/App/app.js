3f5bd682d730c1aab9fbc25ae8c8914c4edbd718
Devyn Bartell <devynbartell@MacBook-Pro-2.local>
2015-10-31 12:10:19 -0600

    added app.js to index file and fixed the route situation

--- public/App/app.js
+++ public/App/app.js
@@ -7,23 +7,14 @@ angular.module('openChairApp', ['ui.router', 'ui.materialize'])
   $stateProvider
   .state('home', {
     url: '/home',
-    templateUrl: './Routes/Home/HomeTmpl.html',
+    templateUrl: 'App/Routes/Home/HomeTmpl.html',
     controllers: 'HomeCtrl'
   })
   .state('search', {
     url: '/search',
-    templateUrl: './routes/search/searchTmpl.html',
+    templateUrl: 'App/Routes/search/searchTmpl.html',
     controllers: 'searchCtrl'
   })
-  .state('businessProfile', {
-      url: '/search/business/:id',
-      templateUrl: './routes/business/businessTmpl.html',
-      controllers: 'businessProfileCtrl'
-  })
-  .state('userProfile', {
-      url: '/user/:id',
-      templateUrl: './routes/user/userTmpl.html',
-      controllers: 'userCtrl'
-  })
   
+
 })
