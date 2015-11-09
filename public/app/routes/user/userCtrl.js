5221530b92996bada1f8d6abd98ff2af6df5e677
Stephen Brinkworth <xoman5144@gmail.com>
2015-11-09 15:50:53 -0700

    commented all route auth check so we can stillEdit

--- public/app/routes/user/userCtrl.js
+++ public/app/routes/user/userCtrl.js
@@ -1,11 +1,11 @@
 angular.module('openChairApp')
 
 .controller('userCtrl', function($scope, user, appointments, loginService, $location){
-  loginService.getUserName().then(function(res) {
-    if (!res.data._id) {
-      $location.path('#/home');
-    }
-  });
+  // loginService.getUserName().then(function(res) {
+  //   if (!res.data._id) {
+  //     $location.path('#/home');
+  //   }
+  // });
   $scope.user = user;
   $scope.appointments = appointments;
 });
