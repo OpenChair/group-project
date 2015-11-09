cb16a7421ffaf7ecb8c9ea674b9de2f870d4e142
Stephen Brinkworth <xoman5144@gmail.com>
2015-11-09 11:32:09 -0700

    got auth route paths working so far

--- public/app/routes/businessDash/businessDashCtrl.js
+++ public/app/routes/businessDash/businessDashCtrl.js
@@ -1,7 +1,13 @@
-angular.module('openChairApp').controller('businessDashCtrl', function($scope, businessService) {
+angular.module('openChairApp').controller('businessDashCtrl', function($scope, businessService, loginService, $location) {
+	loginService.getBusinessName().then(function(res) {
+	  if (!res) {
+	    $location.path('#/home');
+	  }
+	});
+
 	$scope.editHours=function(hours){
 		businessService.editHours(hours).then(function(res){
-			if(res)alert('update completed')
-		})
-	}
+			if(res)alert('update completed');
+		});
+	};
 });
--- public/app/routes/user/userCtrl.js
+++ public/app/routes/user/userCtrl.js
@@ -1,7 +1,12 @@
 angular.module('openChairApp')
 
-.controller('userCtrl', function($scope, user, appointments){
-  $scope.user =
+.controller('userCtrl', function($scope, user, appointments, loginService, $location){
+  loginService.getUserName().then(function(res) {
+    if (!res) {
+      $location.path('#/home');
+    }
+  });
+  $scope.user = user;
   $scope.appointments = appointments;
 
 });
