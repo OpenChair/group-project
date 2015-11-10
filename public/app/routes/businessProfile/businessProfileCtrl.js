61ddafc6c851a29bf4250a734082950ce0b98de0
cyrusholt <cyruslholt@gmail.com>
2015-11-10 14:08:12 -0700

    dropdownbusiness

--- public/app/routes/businessProfile/businessProfileCtrl.js
+++ public/app/routes/businessProfile/businessProfileCtrl.js
@@ -34,4 +34,12 @@ angular.module('openChairApp')
        }
     });
 
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+      
+
+
 });
