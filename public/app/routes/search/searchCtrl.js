8b5245c036c5856992e59d4a04c9d5d527354bb8
cyrusholt <cyruslholt@gmail.com>
2015-11-16 09:41:30 -0700

    user

--- public/app/routes/search/searchCtrl.js
+++ public/app/routes/search/searchCtrl.js
@@ -7,11 +7,3 @@ angular.module('openChairApp')
   });
   });
 });
-
-  .controller('searchCtrl', function($scope, businessService, searchCriteria) {
-    businessService.getBusinesses().then(function(response) {
-      businessService.getFilterdBusinesses(searchCriteria.radius, searchCriteria.lat, searchCriteria.lon).then(function(response) {
-        $scope.businesses = response;
-      });
-    });
-  });
